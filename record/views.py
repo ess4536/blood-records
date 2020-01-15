2203e7ac5505c490f8dc9e89a8754c0f73ad4cc6
ym_ci-g <56593656+ess4536@users.noreply.github.com>
2020-01-15 18:09:29 +0900

    #18, #29, #31 fixed mail

--- record/views.py
+++ record/views.py
@@ -1,6 +1,31 @@
+import logging
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.views import generic
+from .forms import InquiryForm
+from django.contrib import messages
 
 # Create your views here.
+logger = logging.getLogger(__name__)
+
+
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm
+    success_url = reverse_lazy('record:inquiry')
+
+    def get_form(self, form_class=None):
+        if 'name' in self.request.POST:
+            form_data = self.request.POST
+        else:
+            form_data = self.request.session.get('form_data', None)
+        return self.form_class(form_data)
+
+    def from_valid(self, form):
+        form.send_email()
+        messages.success(self.request, 'メッセージを送信しました')
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super(InquiryView, self).form_valid(form)
