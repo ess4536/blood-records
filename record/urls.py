b930b0f6013887abe9a0d52bcac7fdb3e1bcfa50
ym_ci-g <56593656+ess4536@users.noreply.github.com>
2020-01-27 18:27:03 +0900

    add create

--- record/urls.py
+++ record/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('record-detail/', views.RecordDetailView.as_view(), name="record_detail"),
     path('record-detail/<int:pk>/', views.RecordDetailNextView, name="record_detail_next"),
     path('record-create/', views.RecordCreateView.as_view(), name="record_create"),
+    path('record-update/<int:pk>/', views.RecordUpdateView.as_view(), name="record_update"),
 ]
 
