20e7cbdb63b27d8d44a407aff298d86b8f71eb35
ym_ci-g <56593656+ess4536@users.noreply.github.com>
2020-01-27 12:32:22 +0900

    add detailnext

--- record/urls.py
+++ record/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
     path('inquiry/', views.InquiryView.as_view(), name="inquiry"),
     path('record-list/', views.RecordListView.as_view(), name="record_list"),
-    path('record-detail/<int:pk>', views.RecordDetailView.as_view(), name="record_detail"),
+    path('record-detail/', views.RecordDetailView.as_view(), name="record_detail"),
+    path('record-detail/<int:pk>/', views.RecordDetailNextView, name="record_detail_next"),
     path('record-create/', views.RecordCreateView.as_view(), name="record_create"),
 ]
 
