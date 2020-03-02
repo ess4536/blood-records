35431ea0291b52c4afcd3c600259f6a590e2bfec
ym_ci-g <56593656+ess4536@users.noreply.github.com>
2020-03-02 12:48:05 +0900

    add api/

--- blood_records/urls.py
+++ blood_records/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('record.urls')),
     path('account/', include('allauth.urls')),
+    path('api/', include('api.urls')),
 ]
