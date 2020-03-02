a58b4402fc813ba3af92ff67098f026985e8d847
ym_ci-g <56593656+ess4536@users.noreply.github.com>
2020-03-02 12:26:10 +0900

    add sheet api

--- api/permissions.py
+++ api/permissions.py
@@ -1,3 +1,9 @@
-from django.db import models
+from rest_framework import permissions
 
-# Create your models here.
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """ Custom permission to only allow owners of an object to edit it. """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
--- api/views.py
+++ api/views.py
@@ -3,22 +3,26 @@ from django.http import Http404
 from django.views.decorators.csrf import csrf_exempt
 from record.models import Sheet
 from .serializers import SheetSerializer
+from .permissions import IsOwnerOrReadOnly
 
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework import viewsets
 
 # Create your views here.
 
-class SheetList(generics.ListCreateAPIView):
-    """ List all sheet, or create a new sheet. """
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'sheets': reverse('sheet-list', request=request, format=format),
+    })
 
+class SheetViewSet(viewsets.ModelViewSet):
+    """
+        This viewset automatically provides `list`, `create`, `retrieve`,
+        `update` and `destroy` actions.
+    """
     queryset = Sheet.objects.all()
     serializer_class = SheetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-class SheetDetail(generics.RetrieveUpdateDestroyAPIView):
-    """ Retrieve, update or delete a sheet. """
-
-    queryset = Sheet.objects.all()
-    serializer_class = SheetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
